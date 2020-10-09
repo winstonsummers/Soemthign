76db4f3a263b34bddd0ce2020dcc91f505245f05
Winston Summers <wsummers@zulily.com>
2020-10-09 15:58:27 -0700

    basic scroll container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, MouseEvent } from 'react';
 import './App.css';
 import Button from './ui/shared/Button';
+import ScrollContainer from './ui/shared/ScrollContainer';
 
 function App() {
   const [disabled, setDisabled] = useState(false)
@@ -10,16 +11,22 @@ function App() {
   }
   return (
     <div className="App">
-      <Button 
-        text="hello"
-        onClick={onClick}
-        disabled={disabled}
-      />
-      <Button
-        text="reset"
-        onClick={onClick}
-        disabled={!disabled}
-      />
+      <ScrollContainer 
+        height={50}
+        width={150}
+        direction='vertical'
+      >
+        <Button 
+          text="hello"
+          onClick={onClick}
+          disabled={disabled}
+        />
+        <Button
+          text="reset"
+          onClick={onClick}
+          disabled={!disabled}
+        />
+      </ScrollContainer>
     </div>
   );
 }
--- src/ui/shared/ScrollContainer/index.tsx
+++ src/ui/shared/ScrollContainer/index.tsx
@@ -1,3 +1,57 @@
-export default () => {
-  console.log('this is a placeholder')
+import React from "react";
+
+interface IScrollContainer {
+  direction?  : "horizontal" | "vertical" | "both" | "auto" | "hidden"
+  height?     : string | number
+  width?      : string | number
+  className?  : string
+  overlay?    : boolean
 }
+
+const ScrollContainer: React.FC<IScrollContainer> = ({
+  children,
+  className,
+  direction = "auto",
+  height = "auto",
+  width = "auto",
+  overlay = false
+}) => {
+
+  const scrollStyling: any = {
+    scrollBehavior: "smooth",
+    height,
+    width,
+  };
+
+  const scrollValue = overlay ? 'overlay' : 'scroll'
+
+  switch (direction) {
+    case "horizontal":
+      scrollStyling["overflowX"] = scrollValue;
+      scrollStyling["overflowY"] = "hidden";
+      break;
+    case "vertical":
+      scrollStyling["overflowY"] = scrollValue;
+      scrollStyling["overflowX"] = "hidden";
+      break;
+    case "both":
+      scrollStyling["overflow"] = scrollValue;
+      break;
+    case "auto":
+      scrollStyling["overflow"] = "auto";
+      break;
+    case "hidden":
+      scrollStyling["overflow"] = "hidden";
+      break;
+  }
+
+  // TODO: add logic for scroll to bottom and such
+
+  return (
+    <div style={scrollStyling} className={className}>
+      {children}
+    </div>
+  );
+};
+
+export default ScrollContainer;
